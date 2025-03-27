08af1d3734f5874a9a443cfeea68f3c9053f5d77
pjroggy <pierrejeanroggy@gmail.com>
2025-03-27 16:43:38 +0100

    initalize db connexion with credentials

---
+++ rhum-a-ranger/src/main/java/projetPj/rhum_a_ranger/RhumARangerApplication.java
@@ -0,0 +1,13 @@
+package projetPj.rhum_a_ranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhumARangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhumARangerApplication.class, args);
+	}
+
+}
---
+++ rhum-a-ranger/src/test/java/projetPj/rhum_a_ranger/RhumARangerApplicationTests.java
@@ -0,0 +1,13 @@
+package projetPj.rhum_a_ranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhumARangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
