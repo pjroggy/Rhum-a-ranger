ce3fb22100422af54168226e726203936a2fadbe
pjroggy <pierrejeanroggy@gmail.com>
2025-04-02 16:53:15 +0200

    add rhum controler

--- rhum-a-ranger/src/main/java/projetPj/rhum_a_ranger/rhum/RhumController.java
+++ rhum-a-ranger/src/main/java/projetPj/rhum_a_ranger/rhum/RhumController.java
@@ -1,4 +1,58 @@
 package projetPj.rhum_a_ranger.rhum;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rhums")
+@RequiredArgsConstructor
 public class RhumController {
+
+    private final RhumService rhumService;
+
+    @GetMapping
+    public List<Rhum> getAllRhums() {
+        return rhumService.getAllRhums();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rhum> getRhumById(@PathVariable Long id) {
+        return rhumService.getRhumById(id)
+                .map(rhum -> ResponseEntity.ok().body(rhum))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Rhum createRhum(@RequestBody Rhum rhum) {
+        return rhumService.saveRhum(rhum);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Rhum> updateRhum(@PathVariable Long id, @RequestBody Rhum rhum) {
+        Rhum updatedRhum = rhumService.updateRhum(id, rhum);
+        return updatedRhum != null ?
+                ResponseEntity.ok(updatedRhum) :
+                ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRhum(@PathVariable Long id) {
+        rhumService.deleteRhum(id);
+    }
+
+    @GetMapping("/origin/{origin}")
+    public List<Rhum> getRhumsByOrigin(@PathVariable String origin) {
+        return rhumService.getRhumsByOrigin(origin);
+    }
+
+    @GetMapping("/search")
+    public List<Rhum> searchRhumsByName(@RequestParam String keyword) {
+        return rhumService.searchRhumsByName(keyword);
+    }
 }
